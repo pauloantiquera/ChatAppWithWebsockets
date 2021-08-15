5fda160cdeed4d6ad55f66919631c74dd93c9997
pauloantiquera <paulo@antiquera.net>
2021-08-14 22:31:12 -0300

    refactor(chat): :recycle: Changes the main and template files to remove the hard coded http server attributes
    Both the server address and template file coulde be changed in the command line by -addr="" and -template="" flags. If the flags are not set they assume the default values addr=":8080" and template="chat.html".

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,22 +20,24 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
 
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
-	templateFile := "chat.html"
-	httpServerDescription := ":8080"
+	var templateFile = flag.String("template", "chat.html", "The template file for the chat page.")
+	var addr = flag.String("addr", ":8080", "The addt of the application.")
+	flag.Parse()
+
 	r := newRoom()
 
-	log.Printf("Serving the template file %s at %s\n", templateFile, httpServerDescription)
+	log.Printf("Serving the template \"%s\" at %s\n", *templateFile, *addr)
 
-	http.Handle("/", &templateHandler{filename: templateFile})
+	http.Handle("/", &templateHandler{filename: *templateFile})
 	http.Handle("/room", r)
 
 	go r.run()
 
-	if err := http.ListenAndServe(httpServerDescription, nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAnServe:", err)
 	}
 }
